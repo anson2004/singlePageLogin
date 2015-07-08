78e168f1f6306f3214e02d2b7ccf3d2004b132e2
yuting <yuting1029@hotmail.com>
2015-07-08 17:40:41 +0300

    first commit

--- tut1/ViewController.m
+++ tut1/ViewController.m
@@ -9,6 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (nonatomic, strong) NSString *username;
+
+@property (nonatomic, strong) NSString *password;
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UILabel *notificationLabel;
 
 @end
 
@@ -17,11 +24,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.username = @"yiqing";
+    self.password = @"pw";
+    
+    NSLog(@"%@%@",self.username,self.password);
+
+    self.passwordTextField.secureTextEntry = true;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)loginPressed:(id)sender {
+    
+    BOOL isUsernameOK = [self.username isEqualToString:[self.usernameTextField text]];
+    BOOL isPasswordOK = [self.password isEqualToString:[self.passwordTextField text]];
+    
+    if(isUsernameOK && isPasswordOK ){
+        NSLog(@"login ok");
+        [self.notificationLabel setText:@"Login success, welcome to the page"];
+    } else {
+        NSLog(@"login fail");
+        [self.notificationLabel setText:@"Login fail, username/pasword incorrect"];
 
+    }
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing:TRUE];
+}
 @end
